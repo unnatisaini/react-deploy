65235acfa04f85978f4b7b4f2f9d5e1456ccca70
shivaniwe2code <vijendra.patel.we2code@gmail.com>
2022-09-29 18:27:17 +0530

    backpush

--- server1/database.js
+++ server1/database.js
@@ -1,11 +1,12 @@
 var mysql = require('mysql');
 
 var conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "apnaorga_w2cadmin"
-}); 
+            host: "103.117.212.187",
+            user: "apnaorga_jbifs",
+            password: "JU3}?RSuSU(%",
+            database: "apnaorga_employee_management"
+});
+
 conn.connect(function(err) {
   if (err) throw err;
   console.log('Database is connected successfully !');
