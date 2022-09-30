788b30fbc57c369110bd2ebf45f34c09c059e1ab
shivaniwe2code <vijendra.patel.we2code@gmail.com>
2022-09-30 16:42:05 +0530

    addddsnn

--- client/src/MyComponent/common/Header.jsx
+++ client/src/MyComponent/common/Header.jsx
@@ -7,18 +7,18 @@ const Header = () => {
   let navigate = useNavigate();
   const signOut = () =>{
     localStorage.setItem("authenticated",null);
-    navigate("/");
+    navigate("/EmployeeManagement/");
   }
   return (
     <div>
-      <Nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 mb-3">
-        <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">
-          Company name
+      <Nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 mb-3 d-flex ">
+        <a className="navbar-brand col-sm-6 col-md-10 mr-0" href="#">
+          We2Code
         </a>
         {/* <input className="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"> */}
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap">
-          <button className="btn btn-sm btn-outline-secondary" onClick={signOut} >
+          <button className="btn btn-md btn-outline-light text-dark" onClick={signOut} >
           Sign out </button>
           </li>
         </ul>
